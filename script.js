8c7217428f67a077fcf993845369ff7a6da1b917
NRV <nicoleta@redsparq.eu>
2025-05-18 16:28:36 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+window.onload = function () {
+  alert("😊 Thank you for finding my GitHub page!\nYou might be interested in the open roles I'm recruiting.\nCheck them out here: https://www.careers-page.com/redsparq");
+}
